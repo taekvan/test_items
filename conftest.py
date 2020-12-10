799845c15913195794f10f8b76ad1f16eba8c359
taekvan <taekvandovan@gmail.com>
2020-12-10 23:56:32 +0300

    possible to use multiple languges, not fixed

--- conftest.py
+++ conftest.py
@@ -11,25 +11,11 @@ def pytest_addoption(parser):
 def browser(request):
     user_language = request.config.getoption("language")
     browser = None
-    if 	user_language == "ru":
-        print("\nstart  Russian version of chrome browser for test..")
-        options = Options()
-        options.add_experimental_option('prefs', {'intl.accept_languages': user_language})
-        browser = webdriver.Chrome(options=options)
-    elif user_language == "en":
-        print("\nstart English version of chrome browser for test..")
-        options = Options()
-        options.add_experimental_option('prefs', {'intl.accept_languages': user_language})
-        browser = webdriver.Chrome(options=options)
-    elif user_language == "es":
-        print("\nstart Espaniol version of chrome browser for test..")
-        options = Options()
-        options.add_experimental_option('prefs', {'intl.accept_languages': user_language})
-        browser = webdriver.Chrome(options=options)
-    else:
-        raise pytest.UsageError("--language should be ru, en, or es")
+    options = Options()
+    options.add_experimental_option('prefs', {'intl.accept_languages': user_language})
+    browser = webdriver.Chrome(options=options)
     yield browser
     print("\nquit browser..")
-    #browser.quit()
+    browser.quit()
 
 
--- test_items.py
+++ test_items.py
@@ -1,6 +1,7 @@
 link = "http://selenium1py.pythonanywhere.com/catalogue/coders-at-work_207/"
 
 
-def test_guest_should_see_login_link(browser):
+def test_client_should_see_add_to_basket_button(browser):
     browser.get(link)	
-    browser.find_element_by_class_name("btn-add-to-basket")
+    button = browser.find_element_by_class_name("btn-add-to-basket")
+    assert button!=None, "No such element"
